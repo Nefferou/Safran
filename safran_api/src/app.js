2356cc89218df7f3d3cee216946c7a405690e267
arseid <yvesrobert.estrada@gmail.com>
2025-08-21 23:52:55 +0200

    Remove access to all routes except auth ones for unauthorized users

--- safran_api/src/app.js
+++ safran_api/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const authRoutes = require('./routes/v1/auth.routes');
 const notFound = require('./middlewares/not_found');
 const errorHandler = require('./middlewares/error_handler');
+const authJwt = require('./middlewares/auth_jwt');
 const env = require('./config/env');
 const { metricsRouter } = require('./instrumentation/metrics');
 
@@ -19,7 +21,9 @@ if (env.metricsEnabled) {
 // Health
 app.get('/healthz', (req, res) => res.status(200).json({ status: 'ok' }));
 
-// API v1
+// API v1 routes
+app.use('/api/v1/auth', authRoutes);
+app.use(authJwt);
 app.use('/api', routes);
 
 // 404 & Error Handling
--- safran_api/src/routes/v1/index.js
+++ safran_api/src/routes/v1/index.js
@@ -1,9 +1,7 @@
 const { Router } = require('express');
 const users = require('./users.routes');
-const auth = require('./auth.routes');
 
 const router = Router();
 router.use('/users', users);
-router.use('/auth', auth);
 
 module.exports = router;
